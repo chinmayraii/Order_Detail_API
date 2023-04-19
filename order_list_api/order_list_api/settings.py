b20182ab55f9c02e1c12f6a8d5887d2df19d567e
chinmayraii <chinmayrai75@gmail.com>
2023-04-19 12:25:35 +0530

    connect with aws rds

--- order_list_api/order_list_api/settings.py
+++ order_list_api/order_list_api/settings.py
@@ -76,12 +76,26 @@ WSGI_APPLICATION = 'order_list_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'first_test_db',
+        'HOST': '****',
+        'PORT': 5432,
+        'USER': '*****',
+        'PASSWORD': '*****'
     }
 }
+
 
 
 # Password validation
